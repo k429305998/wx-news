4c4e0713d1c17b8f2bdc0e8b1257b9688823a689
chenyanrui <260464538@qq.com>
2017-10-18 09:15:44 +0800

    template data

--- pages/news/news.js
+++ pages/news/news.js
@@ -1,48 +1,21 @@
 // pages/news/news.js
+var newsData = require('../../data/news-data.js');
 Page({
-
   /**
    * 页面的初始数据
    */
-  data: {
-    newsData:[
-        {
-            date: "Oct 14 2017",
-            title: "话题：富坚义博涉嫌临摹构图风波",
-            imgSrc: "/images/news/cartoon.jpg",
-            avatar: "/images/avatar/1.png",
-            content: "作为漫画作品当中典型的“有生之年”系列，《全职猎人》现在能否更新，完全是看富坚义博的脸色了。虽然说老贼在上次休刊的时候表示今年年底之前还会回来，但是直到今天也没有太多的动静。",
-            reading: "2212",
-            collection: "196",
-            author: "腾讯动漫"
-        },
-        {
-            title: "2018～2020年 一年比一年更旺的三大生肖",
-            content: "人在运势差的时候，尽量少出门，很容易因为一些意外破财；而人在鸿运当头的时候，一定要出去多多社交，说不定在什么时候就发现了商机，得到了横财。下面就跟大家讲一下未来三年运势如虹的三大生肖，一起来看看哪些生肖如此幸运吧！",
-            imgSrc: "/images/news/star.jpg",
-            reading: 462,
-            collection: 218,
-            author: "腾讯星座",
-            date: "Oct 14 2017",
-            avatar: "/images/avatar/2.png"
-        },{
-            title: "三星走出Note7阴霾？三季度利润或翻三倍",
-            content: "在国内兴趣阅读平台一片“三星药丸”的唱衰声中，三星电子对外公布了其2017年第三季度财报预期数据，而从数据来看，三星电子第三季度运营利润再创新纪录，实现约合128.1亿美元的运营利润，同比暴增179%。#说三星药丸的，惊不惊喜，意不意外？",
-            imgSrc: "/images/news/mobile.jpg",
-            reading: 602,
-            collection: 192,
-            author: "腾讯数码",
-            date: "Oct 14 2017",
-            avatar: "/images/avatar/3.png",
-        }
-    ]
+  data: {    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // this.data.newsData = newsData.newsList;
+    this.setData({
+      newsData: newsData.newsList
+    })
   },
 
   /**
